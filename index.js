7ef6e127f6f9e4aa13114fb8607fff20f5c4f26a
Sumin-Silver <silver630e@gmail.com>
2025-03-15 12:41:16 -0700

    added level

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var clicker = document.getElementById("clicker")
 var counter = document.getElementById("counter")
 
 var wealth = 0
+var level = 0
 var click_value = 1
 
 clicker.addEventListener('click', function()  {
@@ -35,17 +36,21 @@ clicker.addEventListener('click', function()  {
 function pay(upgrade, cost) {
     wealth -= cost
     cost = Math.ceil(cost * 1.2)
+    level++
     counter.innerHTML = wealth
-    upgrade.children[1].innerHTML = "Cost: " + cost
+    upgrade.children[2].innerHTML = "Cost: " + cost
     return cost
 }
 
 // Increase amount of wealth gained per click
 var upgrade1_cost = 50
+var upgrade1_level = 0
 var upgrade1 = document.getElementById("upgrade1")
 upgrade1.addEventListener('click', function() {
     if (wealth >= upgrade1_cost) {
         upgrade1_cost = pay(upgrade1, upgrade1_cost)
+        upgrade1_level++
+        upgrade1.children[1].innerHTML = "Level: " + upgrade1_level
         click_value++
     }
 })
@@ -53,6 +58,7 @@ upgrade1.addEventListener('click', function() {
 
 // auto clicker values
 var upgrade2_cost = 100
+var upgrade2_level = 0
 var auto_clicker_value = 0
 var auto_clicker_speed = 5000
 var click_interval = setInterval(0)
@@ -62,6 +68,8 @@ var upgrade2 = document.getElementById("upgrade2");
 upgrade2.addEventListener('click', function() {
     if (wealth >= upgrade2_cost) {
         upgrade2_cost = pay(upgrade2, upgrade2_cost)
+        upgrade2_level++
+        upgrade2.children[1].innerHTML = "Level: " + upgrade2_level
         auto_clicker_value++
 
         clearInterval(click_interval)
@@ -72,11 +80,14 @@ upgrade2.addEventListener('click', function() {
     }
 })
 
-var upgrade3_cost = 100
+var upgrade3_cost = 1000
+var upgrade3_level = 0
 var upgrade3 = document.getElementById("upgrade3")
 upgrade3.addEventListener('click', function() {
     if (wealth >= upgrade3_cost) {
         upgrade3_cost = pay(upgrade3, upgrade3_cost)
+        upgrade3_level++
+        upgrade3.children[1].innerHTML = "Level: " + upgrade3_level
         auto_clicker_speed -= 100
 
         clearInterval(click_interval)
