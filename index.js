db856f359a15e65129730e40683ca952d3cbf6e2
R3alHumanBe1ng <csadowsk@outlook.com>
2025-03-15 09:26:04 -0700

    nuh uh

--- index.js
+++
@@ -1,3 +0,0 @@
-let script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.7.1.min.js'; // Check https://jquery.com/ for the current version
-document.getElementsByTagName('head')[0].appendChild(script);
