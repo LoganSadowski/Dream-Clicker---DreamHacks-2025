909e734bb2943aff7c4617731aa5d21576e1f45c
Logan Sadowski <logansadowski5@gmail.com>
2025-03-15 10:28:05 -0700

    Animated button

--- index.js
+++ index.js
@@ -3,4 +3,27 @@ var counter = document.getElementById("counter");
 
 clicker.addEventListener('click', function()  {
     counter.innerHTML++;
+
+    let id = null;
+    let i = 0;
+    let size = 60;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+    if (i == 40) {
+        clearInterval(id);
+    } else if (i < 20) {
+        size++;
+        clicker.style.height = size + 'px';
+        clicker.style.width = size + 'px';
+
+        i++;
+    } else {
+        size--;
+        clicker.style.height = size + 'px';
+        clicker.style.width = size + 'px';
+
+        i++;
+    }
+  }
 })
